bb3a14d0df2459640f5d82dd29248946ec7111fa
alu10194039 <aluurs4039@ieselcaminas.org>
2021-10-20 18:32:08 +0200

    Correccion

--- src/exercicis/Exercici1.kt
+++ src/exercicis/Exercici1.kt
@@ -1,16 +1,18 @@
 package exercicis
 
 import java.io.File
+import java.lang.NumberFormatException
 
 fun main(args: Array<String>) {
 
     var f = File.listRoots()[0]
-    var listaFicheros = f.listFiles().sorted()
+    //var listaFicheros = f.listFiles().sorted()
     var entrada = 0
 
     do {
         val s = "Llista de fitxers i directoris del directori " + f.getCanonicalPath()
-        val validez = false
+        val listaFicheros = f.listFiles().sorted()
+        var validez = false
         var num = 1
 
         println(s)
@@ -19,8 +21,8 @@ fun main(args: Array<String>) {
 
         num = 1
 
-        //if (fAux) //DIRECTORIO Y LECTURA contemplar el 0 y que este el numero entreb 0 y 24
-        if (f.exists()){
+        //if (fAux) //DIRECTORIO Y LECTURA contemplar el 0 y que este el numero entre 0 y 24
+        if (f.exists() && f.canRead()){
             if (f.isDirectory){
                 num = 1
                 for (e in f.listFiles().sorted()) {
@@ -34,14 +36,29 @@ fun main(args: Array<String>) {
                         num++
                     }
                 }
-                println()
+
+                print("\nIntrodueix un numero (-1 per acabar): ")
+                entrada = readLine()!!.toInt()
+
+                try {
+                    if (entrada == 0) {
+                        if (f.parentFile != null)
+                            f = File(f.parentFile.path)
+                    }
+                    if (entrada == -1){
+                        validez = true
+                    } else
+                        f = File(listaFicheros[entrada-1].path)
+                } catch (e : IndexOutOfBoundsException){
+                    println("¡Valor introducido NO VALIDO!\n")
+                } catch (a : NumberFormatException){
+                    println("¡No se permiten caracteres, tan solo numeros!")
+                }
+
             }
-        }//val aux = f.listFiles().sorted().get(entrada - 1)
-
-        print("\nIntrodueix un numero (-1 per acabar): ")
-        entrada = readLine()!!.toInt()
-
-        f = File(listaFicheros[entrada-1].path)
+        }else //val aux = f.listFiles().sorted().get(entrada - 1)
+            println("No tienes permiso de lectura/O no es un directorio")
 
     } while (!validez)
+    println("¡GRACIAS POR UTILIZAR NUESTRO EXPLORADOR DE ARCHIVOS!")
 }
